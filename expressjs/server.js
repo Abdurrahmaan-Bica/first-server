b70585f26f5e318a7c48819a2b50853e6deb0461
codelove <abdurrahmaanbica123@gmail.com>
2024-11-22 01:38:28 +0100

    Added a route that returns a list of todos to the client

--- expressjs/server.js
+++ expressjs/server.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// routes
 app.get('/', (req, res) => {
   res.send('Ola Mundo!')
 })
+// route that returns a list of to dos to the client
+app.get('/api/v1/todos',(req,res)=>{
+  res.json([
+    {id:1, todo:"learn express", done:false},
+    {id:2, todo:"learn js", done:true},
+    {id:3, todo:"learn react", done:false}
+  ]
+
+  )
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
