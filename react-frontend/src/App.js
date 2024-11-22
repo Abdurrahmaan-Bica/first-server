3f39dc6819940be6340d05c128d3ea5bd961a907
codelove <abdurrahmaanbica123@gmail.com>
2024-11-22 22:07:01 +0100

    Added useState and useEffect on App.js to help the react consume the REST API so that the list of TODOS be displayed

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,16 +1,33 @@
+import{useEffect, useState} from 'react'
+// useEffect allows us to run a piece of code before the UI is mounted, nesse caso Hello React
+// useState allows us to set the state so that we can work with data
 import './App.css';
 import axios from 'axios'
 
 
 // added proxy : "http://localhost:3000" On package.json to prevent proxy. Any request that goes to /api  it will send to localhost:3000. Vai parecer que tudo esta rodar no mesmo PORT
 function App() {
-  axios.get("/api/v1/todos")
+  
+  const [todos,setTodos] = useState([])
+
+
+    useEffect(()=>{
+      axios.get("/api/v1/todos")
     .then(res =>{
       console.log(res.data)
+      setTodos(res.data)
     }).catch(err=>{
       console.log(err)
     })
-  return <h1>Hello REACT</h1>
+    },[]) // in a nutshell this useEfffect allows us to run whatever is inside, before the UI is mounted
+  return (
+    <div>
+        <h1>Number of TODOS {todos.length}</h1>
+        <ul>
+          {todos.map(t => <li>ID: {t.id} Description: {t.todo} Done: {t.done}</li>)}
+        </ul>
+    </div>
+  )
    
 }
 
