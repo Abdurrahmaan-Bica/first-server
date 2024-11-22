fa4c2ec702102206a8968ed15aef66a5372433de
codelove <abdurrahmaanbica123@gmail.com>
2024-11-22 01:42:04 +0100

    moved todos to an array of objects named todos

--- expressjs/server.js
+++ expressjs/server.js
@@ -2,19 +2,20 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+const todos = [ 
+  {id:1, todo:"learn express", done:false},
+  {id:2, todo:"learn js", done:true},
+  {id:3, todo:"learn react", done:false}
+]
+
 // routes
 app.get('/', (req, res) => {
   res.send('Ola Mundo!')
 })
 // route that returns a list of to dos to the client
 app.get('/api/v1/todos',(req,res)=>{
-  res.json([
-    {id:1, todo:"learn express", done:false},
-    {id:2, todo:"learn js", done:true},
-    {id:3, todo:"learn react", done:false}
-  ]
-
-  )
+  res.json(todos)
 })
 
 
