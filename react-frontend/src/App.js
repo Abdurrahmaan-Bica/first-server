aaca2536a30194da9b4809327ed8524052f557f3
codelove <abdurrahmaanbica123@gmail.com>
2024-11-22 19:19:34 +0100

    added somee code on index.js of the frontend , to get the list of todos from the server

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,7 +1,15 @@
-
 import './App.css';
+import axios from 'axios'
 
+
+// added proxy : "http://localhost:3000" On package.json to prevent proxy. Any request that goes to /api  it will send to localhost:3000. Vai parecer que tudo esta rodar no mesmo PORT
 function App() {
+  axios.get("/api/v1/todos")
+    .then(res =>{
+      console.log(res.data)
+    }).catch(err=>{
+      console.log(err)
+    })
   return <h1>Hello REACT</h1>
    
 }
